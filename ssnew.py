6a3823957144e67ce48c366004c217cbf0f35894
Abhilash Boggavarapu <chaitu.chowdary00@gmail.com>
2020-03-18 18:13:09 -0600

    changes

--- ssnew.py
+++ ssnew.py
@@ -15,7 +15,7 @@ import os
 import numpy as np
 from matplotlib import pyplot as plt
 
-rate2,test_audio = wavfile.read('Test.wav','w')
+rate2,src_audio = wavfile.read('Test.wav','w')
 rate,bg_audio = wavfile.read('bg_Test.wav')
 rate1,fg_audio = wavfile.read('fg_Test.wav')
 
@@ -30,25 +30,40 @@ bg = np.asarray(bg)
 z = []
 y = []
 
-test = test_audio
-testnew = test_audio
+test = src_audio
+testnew = src_audio
 test = np.asarray(test)
 testnew = np.asarray(testnew)
-for i in range(0,len(test_audio),44100):
-    
-    a = np.sum(np.abs(fg[i:i+44100])) 
-    z.append(a)
-    b = np.sum(np.abs(bg[i:i+44100]))
-    y.append(b)
-    if (a>=b):
-        print ('infi')
-        test[i:i+44100] = 1*test[i:i+44100] 
-        # fgnew.append(fg[i:i+44100])
-    if(a<b):     
-        if(len(str(a))==len(str(b)) and (b-a)>a):
-            test[i:i+44100] = 1*test[i:i+44100] 
+ones = np.ones(11500)
+zeros = np.zeros(11500)
+fg_array = []
+bg_array = []
+misc_array = []
+for i in range(0,len(test_audio),11500):
+    countfg = 0
+    countbg = 0
+    countun = 0
+    for j in range(i,i+11500,1):
+        if (fg[i]>bg[i]):
+            countfg=countfg+1
         else:
-            print ('inelse')
-            test[i:i+44100] = 0*test[i:i+44100] 
+            countbg = countbg+1
+
+    if (countfg/11500>0.7):
+        fg_array.append[ones]
+        bg_array.append[zeros]
+        misc_array.append[zeros]    
+        print(countfg)
+    elif(countfg/11500<0.3):
+        fg_array.append[zeros]
+        bg_array.append[ones]
+        misc_array.append[zeros]
+    else:
+        fg_array.append[zeros]
+        bg_array.append[zeros]
+        misc_array.append[ones]
+
+
+testnew = fg_array*testnew
         
-wavfile.write('xyznewzxxc.wav',rate2,test)        
+wavfile.write('xyznewzxxc.wav',rate2,testnew)        
